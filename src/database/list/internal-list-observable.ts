e0e5f26b795ec1b624bbde1ea2ee3bd500baaad5
adriancarriger <hello@adriancarriger.com>
2017-05-24 07:26:26 -0500

    fix(list): sort array before comparing

--- src/database/list/internal-list-observable.ts
+++ src/database/list/internal-list-observable.ts
@@ -69,6 +69,10 @@ export class InternalListObservable<T> extends ReplaySubject<T> {
    * Only calls next if the new value is unique
    */
   uniqueNext(newValue) {
+    // Sort
+    if (this.previousValue) { this.previousValue.sort((a, b) => a.$key - b.$key); }
+    if (newValue) { newValue.sort((a, b) => a.$key - b.$key); }
+
     if (this.updated > 1 || (stringify(this.previousValue) !== stringify(newValue)) ) {
       this.previousValue = newValue;
       this.next(newValue);

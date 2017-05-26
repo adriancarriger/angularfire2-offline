4e99aeb53aed51a8e62c432e577df1e8d20c1f23
adriancarriger <hello@adriancarriger.com>
2017-05-26 18:44:17 -0500

    chore(9.5): copy 9.4, remove `.first` method

---
+++ issues/9.5/README.md
@@ -0,0 +1,11 @@
+# Issue: 9.5
+
+**After adding to the remote gh-pages branch this version will be visible at:
+
+https://adriancarriger.github.io/angularfire2-offline/issues/9.5/dist
+
+## You can test locally by running:
+
+cd issues/9.5
+npm run static-serve
+
---
+++ issues/9.5/src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import {
+  AngularFireOfflineDatabase,
+  AfoListObservable
+} from 'angularfire2-offline/database';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  urlPath = '/issues/9/9-4';
+  read;
+  notRead;
+
+  constructor(private afoDatabase: AngularFireOfflineDatabase) {
+
+    // First query
+    this.query(this.urlPath, 'READ').subscribe((readResultList) => {
+      console.log('\n\nQueries matching read:', readResultList);
+      this.read = readResultList;
+
+       // Second query
+      this.query(this.urlPath, 'NOT_READ').subscribe((unReadList) => {
+        console.log('\n\nQueries matching not read:', unReadList);
+        this.notRead = unReadList;
+      });
+    });
+
+  }
+
+  query(urlPath: string, status: string): Observable<any[]> {
+    return this.afoDatabase.list(urlPath, {
+      query: {
+        orderByChild: 'categoryId',
+        equalTo: status,
+        limitToFirst: 15
+      }
+    });
+  }
+
+}
+

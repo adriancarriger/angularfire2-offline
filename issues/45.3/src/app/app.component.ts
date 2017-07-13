ab38cc6c83f06f5f956691aafcf857baf05ce3a2
adriancarriger <hello@adriancarriger.com>
2017-07-12 19:15:02 -0500

    chore(build): update 45.3

---
+++ issues/45.3/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+import {
+  AngularFireOfflineDatabase,
+  AfoListObservable,
+  AfoObjectObservable
+} from 'angularfire2-offline/database';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  demoBase = '/issues/45/45-1';
+  balance;
+  counter = 0;
+  lastUserId;
+  test;
+  constructor(private afoDatabase: AngularFireOfflineDatabase) {
+    this.getBalanceStream(1);
+  }
+
+  changeBalance() {
+    this.counter++;
+    this.afoDatabase.object(this.demoBase + '/users/' + this.lastUserId + '/balance').set(this.counter);
+  }
+
+  getBalanceStream(userId) {
+    this.lastUserId = userId;
+    this.afoDatabase.object(this.demoBase + '/users/' + userId + '/balance').subscribe(bal => {
+      console.log('a change occured!');
+      this.balance = bal.$value;
+    });
+  }
+}

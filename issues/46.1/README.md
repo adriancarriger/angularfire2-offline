f6a06df936fafc09c4e02d334d8ffd84e326b816
adriancarriger <hello@adriancarriger.com>
2017-07-30 06:40:27 -0500

    chore(46.1): add example

---
+++ issues/46.1/README.md
@@ -0,0 +1,10 @@
+# Issue: 46.1
+
+**After adding to the remote gh-pages branch this version will be visible at:
+
+https://adriancarriger.github.io/angularfire2-offline/issues/46.1/dist
+
+## You can test locally by running:
+
+cd issues/46.1
+npm run static-serve
---
+++ issues/46.1/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import {
+  AngularFireOfflineDatabase,
+  AfoListObservable,
+  AfoObjectObservable
+} from 'angularfire2-offline/database';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  listExample: AfoListObservable<any[]>;
+  objectExample: AfoObjectObservable<any>;
+  constructor(private afoDatabase: AngularFireOfflineDatabase) {
+    this.listExample = this.afoDatabase.list('groceries');
+    this.objectExample = this.afoDatabase.object('car');
+
+    this.listExample
+      .take(1)
+      .subscribe(items => console.log(items));
+  }
+}

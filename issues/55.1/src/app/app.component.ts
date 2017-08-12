053691484e150ebb14332fd7650fa75975eef609
adriancarriger <hello@adriancarriger.com>
2017-08-12 14:44:47 -0500

    chore(55.1): update base

--- issues/55.1/src/app/app.component.ts
+++ issues/55.1/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import jsonFile from './json-file-example.json';
-
 import {
   AngularFireOfflineDatabase,
   AfoListObservable,
@@ -15,23 +13,7 @@ import {
 })
 export class AppComponent {
   groceries: AfoListObservable<any[]>;
-  car: AfoObjectObservable<any>;
   constructor(private afoDatabase: AngularFireOfflineDatabase) {
     this.groceries = this.afoDatabase.list('groceries');
-    this.car = this.afoDatabase.object('car');
-    this.initializeData();
-  }
-
-  /**
-   * Example of adding inital data from a Json file (will work offline)
-   */
-  initializeData() {
-    // Add list data from Json file
-    this.groceries.remove(); // Remove is optional
-    jsonFile.listExample.forEach(listItem => {
-      this.groceries.push(listItem);
-    });
-    // Add object data from Json file
-    this.car.set(jsonFile.objectExample);
   }
 }
